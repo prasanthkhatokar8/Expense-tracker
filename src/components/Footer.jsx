057f3691235ba62d74e12b560f11827c0f29a4fa
Prasanth Khatokar <prasanthkhatokar8@gmail.com>
2025-04-16 19:47:29 +0530

    Intial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="bg-dark p-4 text-center text-white">&copy;2025, All Rights Reserved. Design and Developed by Prasanth</footer>
+    </div>
+  )
+}
+
+export default Footer
